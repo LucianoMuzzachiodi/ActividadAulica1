4d42399394e0ede5a1ff2e3ce7c856c487de90b5
LucianoMuzzachiodi <lucianomuzzachiodi@sonne.com.ar>
2024-03-21 10:47:03 -0300

    Métodos
    Coloqué atributos, constructor, toString en la Clase carta, y en la clase Baraja coloqué atributo y dos métodos

--- src/actividadaulica1/Baraja.java
+++ src/actividadaulica1/Baraja.java
@@ -1,7 +1,27 @@
 
 package actividadaulica1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Baraja {
-    
+     private List<Carta> cartas;
+
+    public Baraja() {
+        cartas = new ArrayList<>();
+        String[] palos = {"espadas", "bastos", "oros", "copas"};
+        for (String palo : palos) {
+            for (int numero = 1; numero <= 12; numero++) {
+                if (numero != 8 && numero != 9) {
+                    cartas.add(new Carta(numero, palo));
+                }
+            }
+        }
+    }
+
+    public void barajar() {
+        Collections.shuffle(cartas);
+    }
 }
