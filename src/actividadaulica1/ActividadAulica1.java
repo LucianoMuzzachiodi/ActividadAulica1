820f37a2da9e52e7a085ea4367bc861f04533664
LucianoMuzzachiodi <lucianomuzzachiodi@sonne.com.ar>
2024-03-21 15:47:12 -0300

    Se modificó el main
    Se le puso contenido al main.

--- src/actividadaulica1/ActividadAulica1.java
+++ src/actividadaulica1/ActividadAulica1.java
@@ -6,7 +6,22 @@ public class ActividadAulica1 {
 
     
     public static void main(String[] args) {
-        
+         Baraja baraja = new Baraja();
+        baraja.barajar();
+
+        System.out.println("Número de cartas disponibles: " + baraja.cartasDisponibles());
+
+        System.out.println("Siguiente carta: " + baraja.siguienteCarta());
+
+        System.out.println("Número de cartas disponibles: " + baraja.cartasDisponibles());
+
+        System.out.println("Dando 5 cartas:");
+        System.out.println(baraja.darCartas(5));
+
+        System.out.println("Número de cartas disponibles: " + baraja.cartasDisponibles());
+
+        baraja.cartasMonton();
+
+        baraja.mostrarBaraja();
     }
-    
 }
